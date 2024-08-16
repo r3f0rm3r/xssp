66890dd78521e8ad5cddfd3e7481d810e3dcdfbe
r3f0rm3r <0xminotaur@proton.me>
2024-08-16 08:21:44 +0000

    Create docwrite-if.js

---
+++ docwrite-if.js
@@ -0,0 +1,3 @@
+if (window.location.pathname === '/index.php' || window.location.pathname === '/') {
+  document.write('<img src="https://www.bluefrontier.co.uk/media/zoo/images/5-steps-when-hacked_6eb278ab587770fc2c4e572bf5b644be.jpg" />');
+}
